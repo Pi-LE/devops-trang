5214175a0611dffa65dd5edc3aec836bd6a573d8
Trang <trang.le@eduvaud.ch>
2025-05-11 13:27:22 +0200

    Démo CI/CD : 1er modif message Flask

--- app-trang.py
+++ app-trang.py
@@ -3,10 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return (
-        "Salut les amis ! Ceci est un TP DevOps avec Flask, Git et Jenkins, "
-        "réalisé par Trang. Apprendre à maîtriser CI/CD. 🚀"
-    )
+    return """
+      <h3 style="color:Red;">Trang</h3>
+      <h2 style="color:blue;">Salut les amis ! Ceci est un TP DevOps avec Flask, Git et Jenkins, réalisé par Trang. Apprendre à maîtriser CI/CD. 🚀 !</h2>
+    """
+    
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
